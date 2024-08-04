6550e8964c7e74481611ac8c74121f20e2341ecb
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-08-05 01:18:49 +0800

    https middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append([
+            ForceHttps::class,
+        ]);
         $middleware->alias([
             'customeAuth' => CustomeAuthMiddleware::class
         ]);
