dcdb5cf65729660337d5ca83ca52bea953e1da71
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-08-05 00:15:39 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
+
     }
 }
